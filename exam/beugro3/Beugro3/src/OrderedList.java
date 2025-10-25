5120990115ed4149b5b1035ca7bd7e4ab7407d60
gutbrot <gutbrod.adam@gmail.com>
2025-10-25 15:28:16 +0200

    4/4
    getFirst, getList mikor használható?

--- exam/beugro3/Beugro3/src/OrderedList.java
+++ exam/beugro3/Beugro3/src/OrderedList.java
@@ -1,3 +1,29 @@
-public class OrderedList {
-    
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class OrderedList<T extends Comparable<T>> {
+    private ArrayList<T> list;
+    private Comparator<T> comp;
+
+    public OrderedList() {
+        this.list = new ArrayList<>();
+        this.comp = new Comp<>();
+    }
+
+    public void push(T t) {
+        list.add(t);
+        Collections.sort(list, comp);
+    }
+
+    public T pop() {
+        if (list.isEmpty())
+            return null;
+        return list.removeLast();
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
+    }
 }
